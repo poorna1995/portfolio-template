b149e5a5c2bc72a6454b97ea0e657f696101bbdf
Moreshwar-wagh <moreshwarswagh@gmail.com>
2023-10-13 14:22:43 +0530

    [added]: Whitwpaper and research data

--- constants/pages/whitepapers/index.ts
+++ constants/pages/whitepapers/index.ts
@@ -1,10 +1,10 @@
 import { ListProductCardProps } from "../products";
-import bluecomImage from "@/public/assets/products/bluecom.png";
-import hivepathImage from "@/public/assets/products/hivepath.png";
-import zelloImage from "@/public/assets/products/zello.png";
-import bluecomCentralImage from "@/public/assets/products/bluecom-central.png";
-import stylerImage from "@/public/assets/products/styler.png";
-import calpadImage from "@/public/assets/products/calpad.png";
+import aiCompute from "@/public/assets/whitepapers/ai-and-compute.png";
+import dataLoss from "@/public/assets/whitepapers/data-loss.png";
+import dataTech from "@/public/assets/whitepapers/data-tech.png";
+import messagingApp from "@/public/assets/whitepapers/messaging-app.png";
+import multiChannel from "@/public/assets/whitepapers/multichannel-ecom.png";
+import transformation from "@/public/assets/whitepapers/transformation.png";
 import bluecomData from "../products/bluecomData";
 import hivepathData from "../products/hivepathData";
 import zelloData from "../products/zelloData";
@@ -13,97 +13,99 @@ import bluecomCentralData from "../products/bluecomCentralData";
 import stylerData from "../products/stylerData";
 
 const whitepapersPageData: {
-	heroTitle: string;
-	heroDescription: string;
-	listData: Array<ListProductCardProps>;
+  heroTitle: string;
+  heroDescription: string;
+  listData: Array<ListProductCardProps>;
 } = {
-	heroTitle: "Whitpapers",
-	heroDescription: "My Comprehensive list of whitepapers across multiple areas of work.",
-	listData: [
-		{
-			title: "Bluecom.ai",
-			description: "E-commerce SaaS for streamlined product life cycle",
-			backgroundColor: "#3F69FE",
-			count: 1,
-			countTextColor: "white",
-			image: bluecomImage,
-			link: "bluecom",
-			website: "https://bluecom.ai",
-			linkTitle: "Bluecom",
-			timeline: "Dec 2022 - Present",
-			detailsPageData: bluecomData,
-		},
-		{
-			title: "Hivepath.io",
-			description: "Professional networking Platform.",
-			backgroundColor: "#6C60BA",
-			count: 2,
-			countTextColor: "white",
-			image: hivepathImage,
-			link: "hivepath",
-			website: "https://hivepath.io",
-			linkTitle: "Hivepath",
-			timeline: "July 2021 - March 2022",
-			detailsPageData: hivepathData,
-		},
-		{
-			title: "Zello.live",
-			description: "Andriod photo widget sharing platform",
-			backgroundColor: "#739459",
-			count: 3,
-			countTextColor: "white",
-			image: zelloImage,
-			link: "zello",
-			website: "https://zello.live",
-			linkTitle: "Zello",
-			timeline: "March 2022 - July 2022",
-			detailsPageData: zelloData,
-		},
-		{
-			title: "Calpad.co",
-			description: "A calendar booking platform for the professionals",
-			backgroundColor: "#D0B160",
-			count: 4,
-			countTextColor: "white",
-			image: calpadImage,
-			link: "calpad",
-			website: "https://calpad.vercel.app",
-			linkTitle: "Calpad",
-			timeline: "Oct 2022 - Dec 2022",
-			detailsPageData: calpadData,
-		},
-		{
-			title: "Bluecom.central",
-			description: "E-commerce SaaS - Lists of stores",
-			backgroundColor: "#123578",
-			count: 5,
-			countTextColor: "white",
-			image: bluecomCentralImage,
-			link: "bluecom-central",
-			website: "https://bluecom.ai",
-			linkTitle: "Bluecom Central",
-			timeline: "Dec 2022 - Present",
-			detailsPageData: bluecomCentralData,
-		},
-		{
-			title: "Styler",
-			description:
-				"Transformer architecture has been a pivotal discovery in past five years of deep learning.",
-			backgroundColor: "#591192",
-			count: 6,
-			countTextColor: "white",
-			image: stylerImage,
-			link: "styler",
-			website: "https://booking.hivepath.io/anoos",
-			linkTitle: "Styler",
-			timeline: "Feb 2022 - July 2022",
-			detailsPageData: stylerData,
-			imageStyles: {
-				maxHeight: "600px",
-				maxWidth: "400px",
-			},
-		},
-	],
+  heroTitle: "Whitpapers",
+  heroDescription:
+    "My Comprehensive list of whitepapers across multiple areas of work.",
+  listData: [
+    {
+      title: "AI Compute-Technical Deep Dive",
+      description: "Explore AI computing hardware and accelerators",
+      backgroundColor: "#3F69FE",
+      count: 1,
+      countTextColor: "#3F69FE",
+      image: aiCompute,
+      link: "bluecom",
+      website: "https://bluecom.ai",
+      linkTitle: "Bluecom",
+      timeline: "Dec 2022 - Present",
+      detailsPageData: bluecomData,
+    },
+    {
+      title: "Data Loss Protection",
+      description:
+        "Application Level Securty whitepaper Front End and Back End",
+      backgroundColor: "#6C60BA",
+      count: 2,
+      countTextColor: "#CC4B44",
+      image: dataLoss,
+      link: "hivepath",
+      website: "https://hivepath.io",
+      linkTitle: "Hivepath",
+      timeline: "July 2021 - March 2022",
+      detailsPageData: hivepathData,
+    },
+    {
+      title: "MultiChannel E-Commerce",
+      description: "Centralize and Streamline Your Multi-Channel Business",
+      backgroundColor: "#945980",
+      count: 3,
+      countTextColor: "#945980",
+      image: multiChannel,
+      link: "zello",
+      website: "https://zello.live",
+      linkTitle: "Zello",
+      timeline: "March 2022 - July 2022",
+      detailsPageData: zelloData,
+    },
+    {
+      title: "Messaging App",
+      description: "End-to-end messaging app development and api documentation",
+      backgroundColor: "#591192",
+      count: 4,
+      countTextColor: "#591192",
+      image: messagingApp,
+      link: "calpad",
+      website: "https://calpad.vercel.app",
+      linkTitle: "Calpad",
+      timeline: "Oct 2022 - Dec 2022",
+      detailsPageData: calpadData,
+    },
+    {
+      title: "Transformers Architecture",
+      description:
+        "Detailed product research on Pre-qualifying a subcontractor and assessing risk",
+      backgroundColor: "#0856B2",
+      count: 5,
+      countTextColor: "#123578",
+      image: transformation,
+      link: "bluecom-central",
+      website: "https://bluecom.ai",
+      linkTitle: "Bluecom Central",
+      timeline: "Dec 2022 - Present",
+      detailsPageData: bluecomCentralData,
+    },
+    {
+      title: "Data Tech Stack ",
+      description: "Data infrastructure architecture designs",
+      backgroundColor: "#CD9047",
+      count: 6,
+      countTextColor: "#CD9047",
+      image: dataTech,
+      link: "styler",
+      website: "https://booking.hivepath.io/anoos",
+      linkTitle: "Styler",
+      timeline: "Feb 2022 - July 2022",
+      detailsPageData: stylerData,
+      imageStyles: {
+        maxHeight: "600px",
+        maxWidth: "400px",
+      },
+    },
+  ],
 };
 
 export default whitepapersPageData;
