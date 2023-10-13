413624aa6d741a7a44bb11e49e422f0a0e35afb2
Moreshwar-wagh <moreshwarswagh@gmail.com>
2023-10-13 19:12:36 +0530

    [added]: Detail page data

--- constants/pages/products/calpadData.ts
+++ constants/pages/products/calpadData.ts
@@ -1,89 +1,80 @@
-import bluecomImage from "@/public/assets/products/bluecom.png";
-import hivepathImage from "@/public/assets/products/hivepath.png";
-import zelloImage from "@/public/assets/products/zello.png";
+import calpadImage from "@/public/assets/products/calpad-media-image.png";
 
-
-const calpadData ={
-	introData: {
-		title: "About Bluecom",
-        buttonTitle: "Visit Website",
-		content: [
-			{
-				paragraph: `Running a multi-channel ecommerce business can be a challenging and tedious process. By adopting a robust multi-channel framework, ecommerce businesses can unlock a plethora of possibilities, ensuring omnipresence in reaching and serving their customers.`,
-				list: {
-					type: "ordered",
-					data: [
-						{ title: `Product Information Management` },
-						{
-							title: `Inventory Management`,
-						},
-						{
-							title: `Purchase Order Management`,
-						},
-						{
-							title: `Warehouse managemenrt`,
-						},
-					],
-				},
-			},
-		],
-	},
-	contributionData: {
-		title: "My Key Contributions",
-		content: [
-			{
-				list: {
-					type: "ordered",
-					data: [
-						{
-							title: `Built end to end Front application and design framework for the bluecom platform.`,
-						},
-						{
-							title: `Played a crucial role in integrating multiple channels `,
-							list: {
-								type: "unordered",
-								data: [
-									{
-										title: `Woocommerce`,
-									},
-									{
-										title: "Shopify",
-									},
-									{
-										title: "etsy",
-									},
-									{
-										title: "Ebay",
-									},
-									{
-										title: "Amazon",
-									},
-									{
-										title: "Stripe",
-									},
-								],
-							},
-						},
-						{
-							title: `Built detailed documentation on the domain specific content`,
-						},
-					],
-				},
-			},
-		],
-	},
-	productMedia: {
-		image: [
-			{
-				src: bluecomImage,
-			},
-			{
-				src: hivepathImage,
-			},
-			{
-				src: zelloImage,
-			},
-		],
-	},
+const calpadData = {
+  introData: {
+    title: "About Bluecom",
+    buttonTitle: "Visit Website",
+    content: [
+      {
+        paragraph: `Running a multi-channel ecommerce business can be a challenging and tedious process. By adopting a robust multi-channel framework, ecommerce businesses can unlock a plethora of possibilities, ensuring omnipresence in reaching and serving their customers.`,
+        list: {
+          type: "ordered",
+          data: [
+            { title: `Product Information Management` },
+            {
+              title: `Inventory Management`,
+            },
+            {
+              title: `Purchase Order Management`,
+            },
+            {
+              title: `Warehouse managemenrt`,
+            },
+          ],
+        },
+      },
+    ],
+  },
+  contributionData: {
+    title: "My Key Contributions",
+    content: [
+      {
+        list: {
+          type: "ordered",
+          data: [
+            {
+              title: `Built end to end Front application and design framework for the bluecom platform.`,
+            },
+            {
+              title: `Played a crucial role in integrating multiple channels `,
+              list: {
+                type: "unordered",
+                data: [
+                  {
+                    title: `Woocommerce`,
+                  },
+                  {
+                    title: "Shopify",
+                  },
+                  {
+                    title: "etsy",
+                  },
+                  {
+                    title: "Ebay",
+                  },
+                  {
+                    title: "Amazon",
+                  },
+                  {
+                    title: "Stripe",
+                  },
+                ],
+              },
+            },
+            {
+              title: `Built detailed documentation on the domain specific content`,
+            },
+          ],
+        },
+      },
+    ],
+  },
+  productMedia: {
+    image: [
+      {
+        src: calpadImage,
+      },
+    ],
+  },
 };
 export default calpadData;
