80c5dd75cc9c713e4d54d0b1513ee554f90d90d9
Moreshwar-wagh <moreshwarswagh@gmail.com>
2023-10-16 21:45:35 +0530

    [updated]: fontSizes, weight, images and margins of about page is added.

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 
 export default function AboutMeComponent() {
-	return (
-		<div className="pt-4 border-t border-black">
-			<h3 className="my-4 text-3xl font-display">About Me</h3>
-			<p className="mb-4 text-lg">
-				A full-stack Software engineer & designer and a creative thinker
-				with 6+ years of industrial experience. Strongly driven towards
-				creating value for business & technology stakeholders, bringing
-				design innovation to the product. Worked in diverse domains
-				ranging from E-Commerce, Consumer Technology, Finance,
-				Networking, Construction & Entertainment Industries. Experience
-				in building end-to-end UI/UX design teams, grow & mentor.
-			</p>
-			<ul className="text-2xl list-disc list-inside">
-				<span className="font-display">
-					Worked in diverse domains ranging from
-				</span>
-				{list.map((item) => (
-					<li key={item} className="text-lg list-item ">
-						{item}
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="pt-4 border-t border-black max-w-4xl mx-auto">
+      <h3 className="my-4 text-2xl font-medium font-display">About Me</h3>
+      <p className="mb-4 text-lg font-normal">
+        A full-stack Software engineer & designer and a creative thinker with 6+
+        years of industrial experience. Strongly driven towards creating value
+        for business & technology stakeholders, bringing design innovation to
+        the product. Worked in diverse domains ranging from E-Commerce, Consumer
+        Technology, Finance, Networking, Construction & Entertainment
+        Industries. Experience in building end-to-end UI/UX design teams, grow &
+        mentor.
+      </p>
+      <ul className="list-disc list-inside">
+        <span className="text-lg font-medium font-display">
+          Worked in diverse domains ranging from
+        </span>
+        {list.map((item) => (
+          <li key={item} className="text-lg font-normal list-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 const list = [
-	"E-Commerce",
-	"Consumer Technology",
-	"Finance",
-	"Networking",
-	"Construction & Entertainment.",
+  "E-Commerce",
+  "Consumer Technology",
+  "Finance",
+  "Networking",
+  "Construction & Entertainment.",
 ];
--- components/home/education.tsx
+++ components/home/education.tsx
@@ -1,58 +1,84 @@
 import Image from "next/image";
 import React from "react";
+import universityLogo from "@/public/assets/aboutme/university-logo.png";
+import codeAcademyLogo from "@/public/assets/aboutme/code-academy-logo.png";
 
 export default function Education() {
-	return (
-		<div className="mt-8 border-t border-black">
-			<h2 className="my-4 text-3xl font-display">Education</h2>
-			{data.map((item, index) => (
-				<EducationItem key={index} data={item} />
-			))}
-		</div>
-	);
+  return (
+    <div className="mt-8 border-t border-black max-w-4xl mx-auto">
+      <h2 className="my-4 text-2xl font-medium font-display">Education</h2>
+      {data.map((item, index) => (
+        <EducationItem key={index} data={item} />
+      ))}
+    </div>
+  );
 }
 const EducationItem = ({
-	data,
+  data,
 }: {
-	data: {
-		institution: string;
-		institutionLogo: string;
-		degree: string;
-		timePeriod: string;
-		description: string;
-	};
+  data: {
+    institution: string;
+    institutionLogo: String;
+    degree: string;
+    timePeriod: string;
+    description: string;
+  };
 }) => {
-	const { institution, institutionLogo, degree, timePeriod, description } =
-		data;
-	return (
-		<div className="grid grid-cols-12">
-			<div className="col-span-1">
-				<Image
-					src={institutionLogo}
-					alt="logo"
-					width={40}
-					height={40}
-				/>
-			</div>
-			<div className="col-span-11">
-				<div className="flex items-center justify-between">
-					<h3 className="text-2xl font-display">{institution}</h3>{" "}
-					<span>{timePeriod}</span>
-				</div>
-				<h4 className="text-xl">{degree}</h4>
-				<p className="text-lg">{description}</p>
-			</div>
-		</div>
-	);
+  const { institution, institutionLogo, degree, timePeriod, description } =
+    data;
+  return (
+    <div className="grid grid-cols-12 my-4">
+      <div className="col-span-1">
+        <Image src={institutionLogo} alt="logo" width={40} height={40} />
+      </div>
+      <div className="col-span-11">
+        <div className="flex items-center justify-between">
+          <h3 className="text-xl font-medium font-display">{institution}</h3>{" "}
+          <span
+            className="text-base text-lg font-medium"
+            style={{
+              color: "#535353",
+            }}
+          >
+            {timePeriod}
+          </span>
+        </div>
+        <h4
+          className="text-base font-medium mb-2 mt-1"
+          style={{
+            color: "#535353",
+          }}
+        >
+          {degree}
+        </h4>
+        <p
+          className="text-lg font-normal"
+          style={{
+            color: "#535353",
+          }}
+        >
+          <span className="font-bold text-lg">Courses:</span> {description}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 const data = [
-	{
-		institution: "Sri Venkateswara University",
-		institutionLogo: "",
-		degree: "B.tech Major in Chemical Engineering ",
-		timePeriod: "2013-2017",
-		description:
-			"Courses: Data Structures, C, C++, Algorithm, Calculus, Python Programming, Machine Learning, Deep Learning, Natural Language Processing, Computer Vision",
-	},
+  {
+    institution: "Sri Venkateswara University",
+    institutionLogo: universityLogo,
+    degree: "B.tech Major in Chemical Engineering ",
+    timePeriod: "2013-2017",
+    description:
+      "Courses: Data Structures, C, C++, Algorithm, Calculus, Python Programming, Machine Learning, Deep Learning, Natural Language Processing, Computer Vision",
+  },
+  {
+    institution: "Code Academy",
+    institutionLogo: codeAcademyLogo,
+    degree: "Advance Software Immersive Program",
+    timePeriod: "2013-2017",
+    description:
+      "Data Structures, C, C++, Algorithm, Calculus, Python Programming, Machine Learning, Deep Learning, NaturalLanguage Processing, Computer Vision",
+  },
 ];
