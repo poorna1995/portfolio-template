7734247e8158b566bb6895ffe36075e2ea311467
mohit1707 <mehtam2701@gmail.com>
2023-10-19 11:31:23 +0530

    installed and ran prettier

--- components/templates/ListPageTemplate/index.tsx
+++ components/templates/ListPageTemplate/index.tsx
@@ -4,18 +4,15 @@ import { navigationLinks } from "@/constants/navigation";
 import CardsList from "./CardsList";
 
 export default function ListPageTemplate({ pageType }: { pageType: string }) {
-	const page = navigationLinks.find((item) => item.key === pageType);
+  const page = navigationLinks.find((item) => item.key === pageType);
 
-	return (
-		<div>
-			<ListPageHeroComponent
-				title={page?.pageData?.heroTitle}
-				description={page?.pageData?.heroDescription}
-			/>
-			<CardsList
-				data={page?.pageData?.listData || []}
-				pageType={pageType}
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <ListPageHeroComponent
+        title={page?.pageData?.heroTitle}
+        description={page?.pageData?.heroDescription}
+      />
+      <CardsList data={page?.pageData?.listData || []} pageType={pageType} />
+    </div>
+  );
 }
