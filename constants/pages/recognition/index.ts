d249bdd578b31ba1b82c987a765eb131c72ff5a7
Moreshwar-wagh <moreshwarswagh@gmail.com>
2023-10-24 16:56:51 +0530

    [updated]: Employee image updated

--- constants/pages/recognition/index.ts
+++ constants/pages/recognition/index.ts
@@ -1,5 +1,5 @@
 import { ListProductCardProps } from "../products";
-import bestEmployeeAward from "@/public/assets/recognitions/best-employee-award-image.png";
+import bestEmployeeAward from "@/public/assets/recognitions/employee certficate.png";
 import worldQuantImage from "@/public/assets/recognitions/worldquant-challenge-image.png";
 
 import bestEmployeeAwardData from "./bestEmployeeAwardData";
@@ -28,8 +28,8 @@ const recognitionData: {
       countTextColor: "#6C60BA",
       image: bestEmployeeAward,
       linkForWhitepaperDownload:
-      "https://drive.google.com/drive/u/4/folders/1a3UCxoBMQVyIneotKqiZIf0D9uOBbu0H",
-        // "https://drive.google.com/file/d/1goekxXnbvnYgwXan2g4oXhqju4eleipA",
+        "https://drive.google.com/drive/u/4/folders/1a3UCxoBMQVyIneotKqiZIf0D9uOBbu0H",
+      // "https://drive.google.com/file/d/1goekxXnbvnYgwXan2g4oXhqju4eleipA",
       link: "best-employee-award",
       website: "",
       linkTitle: "",
@@ -44,7 +44,8 @@ const recognitionData: {
       count: 1,
       countTextColor: "#6C60BA",
       image: worldQuantImage,
-      linkForWhitepaperDownload: "https://drive.google.com/file/d/1cIcgq0y1v7wdjuQj6qf5l8vOWevLV8ML/view",
+      linkForWhitepaperDownload:
+        "https://drive.google.com/file/d/1cIcgq0y1v7wdjuQj6qf5l8vOWevLV8ML/view",
       link: "worldquant",
       website: "",
       linkTitle: "",
