e7fed96ea8bf29ca623c7f63d9eb95dfb9af3a5d
mohit1707 <mehtam2701@gmail.com>
2023-10-24 20:50:49 +0530

    commented link

--- constants/pages/recognition/index.ts
+++ constants/pages/recognition/index.ts
@@ -7,57 +7,56 @@ import worldquantData from "./worldquantData";
 import { KeyLearningRecognitionProps } from "@/components/templates/RecognitionPageTemplate/KeyLearnings";
 
 export interface RecognitionCardProps extends ListProductCardProps {
-  keyLearningData?: KeyLearningRecognitionProps;
+	keyLearningData?: KeyLearningRecognitionProps;
 }
 
 const recognitionData: {
-  heroTitle: string;
-  heroDescription: string;
-  listData: Array<RecognitionCardProps>;
+	heroTitle: string;
+	heroDescription: string;
+	listData: Array<RecognitionCardProps>;
 } = {
-  heroTitle: "My Recoginition",
-  heroDescription:
-    " ",
-  listData: [
-    {
-      title: "Best Employee Award",
-      description: "Employee Awards",
-      buttonTitle: "Download Certificate",
-      backgroundColor: "#6C60BA",
-      count: 1,
-      countTextColor: "#6C60BA",
-      countBackgroundColor: "#FBFBFB",
-      image: bestEmployeeAward,
-      linkForWhitepaperDownload:
-        "https://drive.google.com/drive/u/4/folders/1a3UCxoBMQVyIneotKqiZIf0D9uOBbu0H",
-      // "https://drive.google.com/file/d/1goekxXnbvnYgwXan2g4oXhqju4eleipA",
+	heroTitle: "My Recoginition",
+	heroDescription: " ",
+	listData: [
+		{
+			title: "Best Employee Award",
+			description: "Employee Awards",
+			buttonTitle: "Download Certificate",
+			backgroundColor: "#6C60BA",
+			count: 1,
+			countTextColor: "#6C60BA",
+			countBackgroundColor: "#FBFBFB",
+			image: bestEmployeeAward,
+			linkForWhitepaperDownload:
+				"https://drive.google.com/drive/u/4/folders/1a3UCxoBMQVyIneotKqiZIf0D9uOBbu0H",
+			// "https://drive.google.com/file/d/1goekxXnbvnYgwXan2g4oXhqju4eleipA",
 
-      link: "best-employee-award",
-      website: "",
-      linkTitle: "",
-      timeline: "",
-      detailsPageData: bestEmployeeAwardData,
-    },
-    {
-      title: "Worldquant",
-      description: "Quantitative Research Summer Training Program",
-      buttonTitle: "Download Certificate",
-      linkForWhitepaperDownload:
-        "https://drive.google.com/file/d/1cIcgq0y1v7wdjuQj6qf5l8vOWevLV8ML",
-      backgroundColor: "#6C60BA",
-      count: 2,
-      countTextColor: "#6C60BA",
-      countBackgroundColor: "",
-      image: worldQuantImage,
-      linkForWhitepaperDownload:
-        "https://drive.google.com/file/d/1cIcgq0y1v7wdjuQj6qf5l8vOWevLV8ML/view",
-      link: "worldquant",
-      website: "",
-      linkTitle: "",
-      timeline: "",
-      detailsPageData: worldquantData,
-    },
-  ],
+			link: "best-employee-award",
+			website: "",
+			linkTitle: "",
+			timeline: "",
+			detailsPageData: bestEmployeeAwardData,
+		},
+		{
+			title: "Worldquant",
+			description: "Quantitative Research Summer Training Program",
+			buttonTitle: "Download Certificate",
+			linkForWhitepaperDownload:
+				"https://drive.google.com/file/d/1cIcgq0y1v7wdjuQj6qf5l8vOWevLV8ML",
+			backgroundColor: "#6C60BA",
+			count: 2,
+			countTextColor: "#6C60BA",
+			countBackgroundColor: "",
+			image: worldQuantImage,
+			// linkForWhitepaperDownload:
+			//   "https://drive.google.com/file/d/1cIcgq0y1v7wdjuQj6qf5l8vOWevLV8ML/view",
+			link: "worldquant",
+			website: "",
+			linkTitle: "",
+			timeline: "",
+			detailsPageData: worldquantData,
+		},
+	],
 };
 
 export default recognitionData;
